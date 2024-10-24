b58a145b7b4b30711c7b999821d1d3d6f8d34fcc
louis.kldzj <louis.kldzj.git@pm.me>
2024-10-24 11:13:48 +0100

    Initial test - expects empty vec before functionality is written

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
 }
+
+const COINS: [usize; 6] = [1, 2, 5, 10, 20, 50];
+
+fn calculate_change(change: usize) -> Vec<usize> {
+    // TODO
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_name() {
+        assert_eq!(calculate_change(15), vec![])
+    }
+}
