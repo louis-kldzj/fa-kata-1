1624c8571930ab4d8ca890440646e0b818031d91
louis.kldzj <louis.kldzj.git@pm.me>
2024-10-24 11:42:50 +0100

    both test cases pass

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,17 @@ fn calculate_change(change: usize) -> Vec<usize> {
     let mut coins_to_return: Vec<usize> = vec![0; 6];
     for (idx, coin) in coins.into_iter().enumerate() {
         if coin <= c_change {
-            c_change -= coin;
-            coins_to_return[idx] += 1;
+            let coin_count: usize = c_change / coin;
+
+            c_change -= coin * coin_count;
+            coins_to_return[idx] += coin_count;
         }
-        if coin == 0 {
+        if c_change == 0 {
+            coins_to_return.reverse();
             return coins_to_return;
         }
     }
+    coins_to_return.reverse();
     coins_to_return
 }
 
@@ -27,7 +31,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_name() {
+    fn works_with_15() {
         assert_eq!(calculate_change(15), vec![0, 0, 1, 1, 0, 0])
+    }
+
+    #[test]
+    fn works_with_45() {
+        assert_eq!(calculate_change(45), [0, 0, 1, 0, 2, 0])
     }
 }
