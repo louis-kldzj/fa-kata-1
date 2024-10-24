729796f7db893a45133aac3a4da4303c23767ca2
louis.kldzj <louis.kldzj.git@pm.me>
2024-10-24 11:17:17 +0100

    still dogy test but ppasses

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ const COINS: [usize; 6] = [1, 2, 5, 10, 20, 50];
 
 fn calculate_change(change: usize) -> Vec<usize> {
     // TODO
+    let mut coins = COINS;
+    coins.reverse();
+    let mut c_change = change;
+    let mut coins_to_return: Vec<usize> = vec![];
+    for coin in coins {
+        if coin <= c_change {}
+    }
     vec![]
 }
 
